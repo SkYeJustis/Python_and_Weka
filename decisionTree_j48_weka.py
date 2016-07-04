725ebce931f941c02151d666fd80ed4464289e8a
SkYe <skyejustis@gmail.com>
2016-07-04 13:12:48 -0400

    Add files via upload

--- decisionTree_j48_weka.py
+++ decisionTree_j48_weka.py
@@ -17,10 +17,21 @@ import weka.core.converters as converters
 data_dir = "data/"
 data = converters.load_any_file(data_dir + "adult.csv")
 data.class_is_last()
-
 #print(data)
 
+# Create train and test sets
+from weka.core.classes import Random
+test, train =  data.train_test_split(0.90, Random(1))
 
+# Check data in datasets
+print(train.num_instances)
+print(test.num_instances)
+
+# Check data in datasets
+print(train.num_attributes)
+print(test.num_attributes)
+
+ 
 # Create classifier
 from weka.classifiers import Classifier
 
@@ -42,35 +53,34 @@ for x in range(0, 4):
     #        print(cls.options)
     
     
-    cls.build_classifier(data)
+    cls.build_classifier(train)
     #       print(cls)
     
     #import weka.plot.graph as graph  
     #graph.plot_dot_graph(cls.graph)
     
     from weka.classifiers import Evaluation
-    from weka.core.classes import Random
-    evl = Evaluation(data)
-    evl.crossvalidate_model(cls, data, 10, Random(1))
+    evl = Evaluation(train)
+    evl.crossvalidate_model(cls, train, 10, Random(1))
     results.update({ x :["option:", opt.get(x), "kappa: ", evl.kappa] })
 
 # View all kappa values and the corresponding options
 print(results)
 
-# ['option:', ['-C', '0.15', '-M', '2'], 'kappa: ', 0.601963928972143] was the best
+# ['option:', ['-C', '0.20', '-M', '2'], 'kappa: ', 0.6013608421053758] was the best
 from weka.classifiers import PredictionOutput
 
 pred_output = PredictionOutput(
 classname="weka.classifiers.evaluation.output.prediction.PlainText", options=["-distribution"])
 
-cls.options =  ["-C", "0.15", "-M", "2"]
+cls.options =  ["-C", "0.20", "-M", "2"]
 
 from weka.classifiers import Evaluation
 from weka.core.classes import Random
-evl = Evaluation(data)
-evl.crossvalidate_model(cls, data, 10, Random(1), pred_output)
+evl = Evaluation(train)
+evl.crossvalidate_model(cls, train, 10, Random(1), pred_output)
 
-# View complete summary of the selected model
+# View complete summary of the selected model on train data
 print(evl.summary())
 
 
@@ -93,4 +103,24 @@ print(cls.graph)
 # 2. Download GraphViz to paste .dot file text and create .dot file as a image 
 # (e.g., .png  .jpeg)
 # http://www.graphviz.org/
+
+
+##  Test model on new data ##
+evl = Evaluation(test)
+evl.crossvalidate_model(cls, test, 10, Random(1), pred_output)
+
+# View complete summary of the selected model on test data
+print(evl.summary())
+# The kappa statistic is 34% in this case,
+# even though the accuracy is 81%.
+# The model does not do significantly better than random chance, 
+# but the model does a fair job in classification accuracy
+
+# View prediction output for model on test data
+print(pred_output.buffer_content())
+
+
+
+
+
 
